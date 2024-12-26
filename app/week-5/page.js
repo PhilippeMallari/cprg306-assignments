2bbf1dbd09a81d8a0951d5a45f1012a5c4b2c333
PhilippeMallari <mallari.philippe@gmail.com>
2024-12-26 22:54:43 +0800

    added into main page.js for rendering on browser

---
+++ app/week-5/page.js
@@ -0,0 +1,16 @@
+// import Counter from "./counter"
+// import Activity from "./activity"
+import NewItem from "./new-item"
+import ControlledComponent from "./controlled-component"
+
+export default function Page() {
+    return <main>
+        <h1 className="text-center text-white">
+            Start of Week 5
+        </h1>
+        {/* <Counter /> */}
+        {/* <Activity /> */}
+        <NewItem />
+        {/* <ControlledComponent /> */}
+    </main>
+}
