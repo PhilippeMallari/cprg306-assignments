8f122b8355e97afc361b22ec255e3a739959e73a
PhilippeMallari <mallari.philippe@gmail.com>
2025-01-06 18:25:50 +0800

    adding page.json for handling lists

---
+++ app/week-6/page.js
@@ -0,0 +1,10 @@
+import CodingDemo from "./coding-demo"
+
+export default function Page() {
+    return <main>
+        <h1 className="text-white">
+            Start of Week 6
+        </h1>
+        <CodingDemo />
+    </main>
+}
